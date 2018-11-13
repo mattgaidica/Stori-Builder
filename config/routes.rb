4a72ff1289303f1d027cbe14add5fe13e9083fd5
Matt Gaidica <matt@gaidi.ca>
2018-11-13 14:46:20 -0500

    Formatting views, need to do edit and forms

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :thoughts
   resources :annotations
   resources :sources
+
+  root to: 'pages#dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
