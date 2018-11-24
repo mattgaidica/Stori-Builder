49aa64d9caeec954c699ce0a142aa783fbf4a904
Matt Gaidica <matt@gaidi.ca>
2018-11-24 12:21:33 -0500

    Importing from PDF reader using regex, authors editable

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -22,7 +22,8 @@ class SourcesController < ApplicationController
   end
 
   def reimport
-    @source.import
+    doc_info = Source.extract_doc(@source.html_file)
+    @source.annotate(doc_info)
     redirect_to edit_source_path(@source)
   end
 
@@ -31,11 +32,13 @@ class SourcesController < ApplicationController
       Dir["#{params[:source_path]}/*.html"].each do |html_file|
         # check for file, if exists skip it, handle re-import elsewhere
         if Source.where(html_file: html_file).empty?
-          doc = Nokogiri::HTML(File.open(html_file))
-          title = doc.css('h1').first.text
-          source = Source.find_or_initialize_by(title: title)
+          doc_info = Source.extract_doc(html_file)
+          source = Source.find_or_initialize_by(title: doc_info[:title])
+          source.authors = doc_info[:authors]
           source.html_file = html_file
-          source.import
+          if source.save
+            source.annotate(doc_info)
+          end
         end
       end
     end
@@ -90,6 +93,6 @@ class SourcesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def source_params
-      params.require(:source).permit(:title, annotations_attributes: [:id, :body])
+      params.require(:source).permit(:title, :authors, annotations_attributes: [:id, :body])
     end
 end
--- app/models/source.rb
+++ app/models/source.rb
@@ -10,17 +10,46 @@ class Source < ApplicationRecord
     return has_similar
   end
 
-  def import
-    doc = Nokogiri::HTML(File.open(self.html_file))
-    self.authors = doc.css('h2').first.text
-    self.pdf_link = doc.css('a').first[:href]
-    if self.save
-      doc.css('p').each do |obj|
-        body = obj.text
-        unless self.has_similar_annotation(body)
-          annotation = Annotation.create(source_id: self.id, body: body)
-        end
+  def annotate(doc_info)
+    doc_info[:annotations].each do |body|
+      unless self.has_similar_annotation(body)
+        annotation = Annotation.create(source_id: self.id, body: body)
       end
     end
+  end
+
+  # def import
+  #   doc = Nokogiri::HTML(File.open(self.html_file))
+  #   self.authors = doc.css('h2').first.text
+  #   self.pdf_link = doc.css('a').first[:href]
+  #   if self.save
+  #     doc.css('p').each do |obj|
+  #       body = obj.text
+  #       unless self.has_similar_annotation(body)
+  #         annotation = Annotation.create(source_id: self.id, body: body)
+  #       end
+  #     end
+  #   end
+  # end
+
+  def self.extract_doc(html_file)
+    doc = Nokogiri::HTML(File.open(html_file))
+
+    regexp = /underline \[page \d\]: (?<text>.+)/
+    title = Time.now.strftime("%Y%m%d_%H%M%S")
+    authors = nil
+    items = doc.css(".item").map {|x| x.text.match(regexp)}
+    items.each_with_index do |item,index|
+      unless item.nil?
+        title = item[:text] if index == 0 
+        authors = item[:text] if index == 1 
+      end
+    end
+
+    regexp = /highlight \[page \d\]: (?<text>.+)/
+    items = doc.css(".item").map {|x| x.text.match(regexp)}
+    annotations = items.compact.map {|x| x[:text]}
+
+    {title: title, authors: authors, annotations: annotations}
   end
 end
