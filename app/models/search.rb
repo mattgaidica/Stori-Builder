54e41e71877e5532cc15ecc8938d7e6fcb8a1f58
Matt Gaidica <matt@gaidi.ca>
2018-11-27 14:02:05 -0500

    Updated search for unique annotations

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,20 +2,19 @@ class Search < ApplicationRecord
 
   def self.for(term)
     results = [];
-    annot_ids = [];
     annot_count = 0;
-    Source.where("title LIKE ? OR authors LIKE ?", "%#{term}%", "%#{term}%").order(created_at: :desc).each do |source|
-      annot_ids = annot_ids | source.annotations.map(&:id)
-      results[annot_count] = source.annotations.map(&:id).to_json
-      annot_count += 1;
-    end
-    Annotation.where("body like ?", "%#{term}%").each do |annotation|
-      unless annot_ids.include? annotation.id
-        annot_ids << annotation.id
-        results[annot_count] = [annotation.id].to_json
+    Source.where("title LIKE ? OR authors LIKE ?", "%#{term}%", "%#{term}%").each do |source|
+      source.annotations.each do |annotation|
+        results[annot_count] = [source.id,annotation.id]
         annot_count += 1;
       end
     end
-    return results
+
+    Annotation.where("body like ?", "%#{term}%").each do |annotation|
+      results[annot_count] = [annotation.source_id,annotation.id]
+      annot_count += 1;
+    end
+
+    return results.sort.reverse
   end
 end
