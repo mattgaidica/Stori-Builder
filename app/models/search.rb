3968b2526bf20bd2c37e216433e889abc579e357
Matt Gaidica <matt@gaidi.ca>
2019-10-09 17:05:06 -0400

    Tooltips working, sidebar done, thoughts working

--- app/models/search.rb
+++ app/models/search.rb
@@ -3,7 +3,7 @@ class Search < ApplicationRecord
   def self.for(term)
     results = [];
     annot_count = 0;
-    Source.where("title LIKE ? OR authors LIKE ?", "%#{term}%", "%#{term}%").each do |source|
+    Source.where("lower(title) LIKE ? OR lower(authors) LIKE ?", "%#{term.downcase}%", "%#{term.downcase}%").each do |source|
       source.annotations.each do |annotation|
         results[annot_count] = [source.id,annotation.id]
         annot_count += 1;
@@ -11,7 +11,7 @@ class Search < ApplicationRecord
     end
     cur_annots = results.map{|x,y| y} # gather all annotation_ids
 
-    Annotation.where("body like ?", "%#{term}%").each do |annotation|
+    Annotation.where("lower(body) like ?", "%#{term.downcase}%").each do |annotation|
       unless cur_annots.index(annotation.id)
         results[annot_count] = [annotation.source_id,annotation.id]
         annot_count += 1;
